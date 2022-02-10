0c7afe340358af6ceece6e77835a85df6b1c8de1
JackJaco <jacob.haara@elev.ga.ntig.se>
2022-02-10 12:26:26 +0100

    uppgift02

--- Uppgift01.php
+++ Uppgift01.php
@@ -1,21 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+<?php
     header("Content-type: text/html; charset=utf-8");
     if(empty($_GET['namn'])){
         echo '<h1>Välkommen!</h1>';
     }
     else{
         $namn=filter_input(INPUT_GET, 'namn');
-        echo "<h1>Välkommen {$namn}!</h1>";
+        echo "<h1>Välkommen till {$namn}s webbsida!</h1>";
         echo "<p>Namnet $namn innehåller ", strlen($namn), " tecken.<p>";
+        echo "<p>Namnet $namn består av ", str_word_count($namn), " ord.";
     }
-</body>
-</html>
