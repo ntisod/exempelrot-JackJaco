3df486c8c29914375cac5904f0105944ba422246
JackJaco <jacob.haara@elev.ga.ntig.se>
2022-05-16 10:33:07 +0200

    style.css fix

--- templates/head.php
+++ templates/head.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrera ny användare</title>
+    <title>Registrera dig här!</title>
     <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <style>
@@ -15,7 +15,7 @@
     <h1>Jacks hemsida</h1>
     <div class="topnav" id="myTopnav">
     <a href="#Hem" class="active">Home</a>
-    <a href="Eegister-form.php">Registrera</a>
+    <a href="Register-form.php">Registrera</a>
     <a href="#Kontakta oss">Contact</a>
     <a href="#Om">About</a>
     <a href="javascript:void(0);" class="icon" onclick="hideTopNav()">
